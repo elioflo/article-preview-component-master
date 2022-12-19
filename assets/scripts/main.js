8174c5e6d55234d50a686cf8f1e836b1af81b4fe
elioflo <eliofloreschoque2357@gmail.com>
2022-12-19 00:05:45 -0300

    Complete README

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,12 @@
 const btn = document.getElementById('btn')
 const toggle = document.getElementsByClassName('toggle')[0]
+const user = toggle.getElementsByClassName('user')[0]
+const contact = toggle.getElementsByClassName('contact')[0]
+const btn_img = btn.getElementsByTagName('img')[0]
 
-btn.addEventListener('click',()=>{
-    toggle.classList.toggle('toggle--dark')
-    toggle.getElementsByClassName('user')[0].classList.toggle('user--clicked')
-    toggle.getElementsByClassName('contact')[0].classList.toggle('contact--clicked')
-    btn.getElementsByTagName('img')[0].src = btn.classList.toggle('toggle__btn--clicked') ? './assets/images/icon-share-white.svg':'./assets/images/icon-share-dark.svg'
+btn.addEventListener('click', () => {
+  toggle.classList.toggle('toggle--dark')
+  user.classList.toggle('user--clicked')
+  contact.classList.toggle('contact--clicked')
+  btn_img.src = btn.classList.toggle('toggle__btn--clicked') ? './assets/images/icon-share-white.svg':'./assets/images/icon-share-dark.svg'
 })
-
