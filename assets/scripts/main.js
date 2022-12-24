5347e83b07968c04212fba659c168aaf0533b0d8
elioflo <eliofloreschoque2357@gmail.com>
2022-12-24 18:12:47 -0300

    Fix accesibility errors. Change img for svg tag, simplify change color button.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,11 +2,10 @@ const btn = document.getElementById('btn')
 const toggle = document.getElementsByClassName('toggle')[0]
 const user = toggle.getElementsByClassName('user')[0]
 const contact = toggle.getElementsByClassName('contact')[0]
-const btn_img = btn.getElementsByTagName('img')[0]
 
 btn.addEventListener('click', () => {
   toggle.classList.toggle('toggle--dark')
   user.classList.toggle('user--clicked')
   contact.classList.toggle('contact--clicked')
-  btn_img.src = btn.classList.toggle('toggle__btn--clicked') ? './assets/images/icon-share-white.svg':'./assets/images/icon-share-dark.svg'
+  btn.classList.toggle('toggle__btn--clicked')
 })
